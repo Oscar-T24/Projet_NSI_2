41870164f4bfb478434b2bda86277c2caa758b09
Oscar-T24 <116503103+Oscar-T24@users.noreply.github.com>
2022-12-17 09:43:24 +0100

    major release ...

--- flou.py
+++ flou.py
@@ -1,9 +1,10 @@
 from PIL import Image
-# NE FONCTIONERA PAS AVEC UINE IMAGE SAS FOND(pour les PNG)
+# NE FONCTIONERA PAS AVEC UNE IMAGE JPEG
+# CREE UNE IMAGE FLOUTÉE E FAISANT LA MOYENNE DES PIXELKS ENVIRONNNANTS
 
-image = Image.open('chien.png')
+image = Image.open('poisson.png')
 taille_init = (image.width,image.height)
-image = image.resize((300,300))
+image = image.resize((round(taille_init[0]/3),round(taille_init[1]/3)))
 
 def flou(image,pixels):
     '''
@@ -32,11 +33,11 @@ def flou2(image): # Ajouter un facteur de floutage
     moyenne_G = 0
     moyenne_V = 0
     
-    for e in range(4,image.width-3):
-        for e2 in range(4,image.height-3):
+    for e in range(8,image.width-8):
+        for e2 in range(8,image.height-8):
             liste_rgb = []         
-            for voisin_x in range(e-2,e+4,2):
-                for voisin_y in range(e2-2,e2+4):
+            for voisin_x in range(e-8,e+9,4):
+                for voisin_y in range(e2-8,e2+9,4):
                      # loiste rgb d'un des 9 pixels
                     r, g, b = rgb_im.getpixel((voisin_x,voisin_y))
                     liste_rgb.append([r,g,b])
@@ -48,9 +49,9 @@ def flou2(image): # Ajouter un facteur de floutage
                 moyenne_G += e4[1]
                 moyenne_V += e4[2]
 
-            moyenne_R = round(moyenne_R / 25)
-            moyenne_G = round(moyenne_G / 25)
-            moyenne_V = round(moyenne_V / 25)
+            moyenne_R = round(moyenne_R / len(liste_rgb)) # 64 pixels qui onnt été choisis
+            moyenne_G = round(moyenne_G / len(liste_rgb))
+            moyenne_V = round(moyenne_V / len(liste_rgb))
             # ----------------------------------------------------------------------------------------
             #print('moyennnes de chaque 9 pixel',moyennes)
             '''
@@ -58,9 +59,11 @@ def flou2(image): # Ajouter un facteur de floutage
                 for voisin_y in range(e2-6,e2+6):
                     rgb_im_final.putpixel((voisin_x, voisin_y), (moyenne_R,moyenne_G,moyenne_V, 0))
             '''
-            rgb_im_final.putpixel((e, e2), (moyenne_R,moyenne_G,moyenne_V, 0))
+            for voisin_x in range(e-8,e+9,4):
+                for voisin_y in range(e2-8,e2+9,4):
+                    rgb_im_final.putpixel((voisin_x, voisin_y), (moyenne_R,moyenne_G,moyenne_V, 24))
+
     rgb_im_final.show()
     rgb_im.show()
     
 flou2(image)
-image.show()
