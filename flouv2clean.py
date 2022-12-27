8e91efa8807b396b8210cce967b0f847959e3a01
Oscar-T24 <116503103+Oscar-T24@users.noreply.github.com>
2022-12-27 14:00:54 +0100

    fini

--- flouv2clean.py
+++ flouv2clean.py
@@ -1,16 +1,15 @@
-from PIL import Image
+from PIL import Image, ImageFilter
+import statistics
 # NE FONCTIONERA PAS AVEC UNE IMAGE JPEG
 # CREE UNE IMAGE FLOUTÉE E FAISANT LA MOYENNE DES PIXELKS ENVIRONNNANTS
 
-image = Image.open('images/poisson.png')
+image = Image.open('images/6.png')
 taille_init = (image.width,image.height)
-image = image.resize((round(taille_init[0]/3),round(taille_init[1]/3))) # resize l'image pour rendre le processus plus rapide
 
-
-def flou(image): 
+def flou(image,rayon): 
     '''
     pixels = nombre de pixels en longeur et largeur
-    img(png) -> img(png, floutée)
+    img(png), int(pixels) -> img(png, floutée)
     fait pour chaque pixel la moyene du pixel et de celle des 8 pixels avoisinants
     '''
     #taille_ini = (image.width,image.height)
@@ -21,38 +20,47 @@ def flou(image):
     moyenne_G = 0
     moyenne_V = 0
     
-    for x in range(1,image.width):
-        for y in range(1,image.height):
+    for x in range(rayon,image.width-(rayon+1)):
+        for y in range(rayon,image.height-(rayon+1)):
 
     # ------------------------PARTIE PARCOURS --------------------------------------------------------
 
     # PARCOUR DE CHAQUE PIXEL : AMBIGUITÉ  : PREND ON LE MOYENNE DE CHAQUE PIXEL OU E FAISANT UN SAUT DE CARRÉ POUR ÉVITER DE FAIRE LA MOYENNE D PIXELS PLUSIEURS FOIS ?
-            liste_rgb = []         
-            for voisin_x in range(x-1,x+2):
-                for voisin_y in range(y-1,y+2):
+            liste_rgb = [0,0,0]        
+            comptage = 0
+            for voisin_x in range(x-rayon,x+(rayon),rayon):
+                for voisin_y in range(y-rayon,y+(rayon),rayon):
                     r, g, b = rgb_im.getpixel((voisin_x,voisin_y))
-                    liste_rgb.append([r,g,b])
+                    liste_rgb[0] += r
+                    liste_rgb[1] += g
+                    liste_rgb[2] += b
+                    comptage += 1
 
             # on obtient une liste de 9 élements(chaque element ayant une composante RGB)
 
             # ------------------------PARTIE MOYENNE DE CHAQUE COMPOSANNT ----------------------------
-
-            # A SYNNTHETISER 
-
-            for rgb in liste_rgb:
-                moyenne_R += rgb[0] 
-                moyenne_G += rgb[1]
-                moyenne_V += rgb[2]
-        
-            moyenne_R = round(moyenne_R / len(liste_rgb)) 
-            moyenne_G = round(moyenne_G / len(liste_rgb))
-            moyenne_V = round(moyenne_V / len(liste_rgb))
+            moyenne = [round((liste_rgb[i])/comptage) for i in range(3)]
+            '''
+            moyenne_R = round(liste_rgb[0] / comptage) 
+            moyenne_G = round(liste_rgb[1] / comptage)
+            moyenne_V = round(liste_rgb[2] / comptage)
+            '''
             # ----------------------------------------------------------------------------------------
             #print('moyennnes de chaque 9 pixel',moyennes)
             
-            rgb_im_final.putpixel((x, y), (moyenne_R,moyenne_G,moyenne_V, 9))
+            rgb_im_final.putpixel((x, y), (moyenne[0],moyenne[1],moyenne[2]))
 
     rgb_im_final.show()
     rgb_im.show()
     
-flou2(image)
+    
+flou(image,8)
+
+# Opens a image in RGB mode
+im = Image.open('images/6.png')
+
+# Blurring the image
+im1 = im.filter(ImageFilter.BoxBlur(4))
+
+# Shows the image in image viewer
+im1.show()
