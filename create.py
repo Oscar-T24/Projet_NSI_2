860683c2de3d5f73c401fda31a97e57ce48eaafd
Oscar-T24 <116503103+Oscar-T24@users.noreply.github.com>
2023-01-05 07:46:23 +0100

    ajouté une fonction variation qui genere une image variante de celle donnée grace à open AI

--- create.py
+++ create.py
@@ -42,3 +42,12 @@ def create():
     with open(file_name, mode="w", encoding="utf-8") as file:
 
         json.dump(response, file)
+'''
+def variation(image):
+    response = openai.Image.create_variation(
+    image=open("corgi_and_cat_paw.png", "rb"),
+    n=1,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+'''
